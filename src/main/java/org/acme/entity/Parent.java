152a8c87b902eb273c6b98233154cf11f43833ee
André Paulino <andrepaulinobtw@gmail.com>
2024-11-18 18:45:34 -0300

    ref: ajusta classe de id entidade parent

--- src/main/java/org/acme/entity/Parent.java
+++ src/main/java/org/acme/entity/Parent.java
@@ -1,26 +1,33 @@
 package org.acme.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+import org.acme.dto.Message;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.acme.dto.Message;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-
 @Entity
 @Table(name = "PARENT", schema = "SCHEMA1")
+@IdClass(ParentId.class)
+@Data
+@ToString
 public class Parent implements Serializable {
 
-    @EmbeddedId
-    private ParentId id;
+    @Id
+    @Column(name = "COMMON_ID_1")
+    private String commonId1;
+
+    @Id
+    @Column(name = "COMMON_ID_2")
+    private String commonId2;
+
+    @Id
+    @Column(name = "COMMON_ID_3")
+    private String commonId3;
 
     @Column(name = "PARENT_COLUMN_1")
     private String parentColumn1;
@@ -39,65 +46,15 @@ public class Parent implements Serializable {
     private List<Child> children = new ArrayList<>();
 
     public static Parent buildFromMessage(Message message) {
-
         var parent = new Parent();
-        parent.setId(new ParentId(message.getId1(), message.getId2(), message.getId3()));
+        parent.setCommonId1(message.getId1());
+        parent.setCommonId2(message.getId2());
+        parent.setCommonId3(message.getId3());
         parent.setParentColumn1(message.getParent1());
         parent.setParentColumn2(message.getParent2());
         parent.setChildren(Child.buildFromMessage(message));
         parent.setParentUpdate(LocalDateTime.now());
 
-
         return parent;
-
     }
-
-    public ParentId getId() {
-        return id;
-    }
-
-    public void setId(ParentId id) {
-        this.id = id;
-    }
-
-    public String getParentColumn1() {
-        return parentColumn1;
-    }
-
-    public void setParentColumn1(String parentColumn1) {
-        this.parentColumn1 = parentColumn1;
-    }
-
-    public String getParentColumn2() {
-        return parentColumn2;
-    }
-
-    public void setParentColumn2(String parentColumn2) {
-        this.parentColumn2 = parentColumn2;
-    }
-
-    public LocalDateTime getParentUpdate() {
-        return parentUpdate;
-    }
-
-    public void setParentUpdate(LocalDateTime parentUpdate) {
-        this.parentUpdate = parentUpdate;
-    }
-
-    public List<Child> getChildren() {
-        return children;
-    }
-
-    public void setChildren(List<Child> children) {
-        this.children = children;
-    }
-
-    @Override
-    public String toString() {
-        return "Parent [id=" + id + ", parentColumn1=" + parentColumn1 + ", parentColumn2=" + parentColumn2
-                + ", parentUpdate=" + parentUpdate + ", children=" + children + "]";
-    }
-
-    
-
 }
--- src/main/java/org/acme/entity/ParentId.java
+++ src/main/java/org/acme/entity/ParentId.java
@@ -1,101 +1,22 @@
 package org.acme.entity;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
-import jakarta.validation.constraints.NotNull;
-
-@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor
 public class ParentId implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
-    @Column(name = "COMMON_ID_1")
     private String commonId1;
-
-    @Column(name = "COMMON_ID_2")
     private String commonId2;
-
-    @Column(name = "COMMON_ID_3")
     private String commonId3;
-
-    public ParentId() {
-    }
 
     public ParentId(@NotNull String commonId1, @NotNull String commonId2, @NotNull String commonId3) {
         this.commonId1 = commonId1;
         this.commonId2 = commonId2;
         this.commonId3 = commonId3;
     }
-
-    public String getCommonId1() {
-        return commonId1;
-    }
-
-    public void setCommonId1(String commonId1) {
-        this.commonId1 = commonId1;
-    }
-
-    public String getCommonId2() {
-        return commonId2;
-    }
-
-    public void setCommonId2(String commonId2) {
-        this.commonId2 = commonId2;
-    }
-
-    public String getCommonId3() {
-        return commonId3;
-    }
-
-    public void setCommonId3(String commonId3) {
-        this.commonId3 = commonId3;
-    }
-
-
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((commonId1 == null) ? 0 : commonId1.hashCode());
-        result = prime * result + ((commonId2 == null) ? 0 : commonId2.hashCode());
-        result = prime * result + ((commonId3 == null) ? 0 : commonId3.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        ParentId other = (ParentId) obj;
-        if (commonId1 == null) {
-            if (other.commonId1 != null)
-                return false;
-        } else if (!commonId1.equals(other.commonId1))
-            return false;
-        if (commonId2 == null) {
-            if (other.commonId2 != null)
-                return false;
-        } else if (!commonId2.equals(other.commonId2))
-            return false;
-        if (commonId3 == null) {
-            if (other.commonId3 != null)
-                return false;
-        } else if (!commonId3.equals(other.commonId3))
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "ParentId [commonId1=" + commonId1 + ", commonId2=" + commonId2 + ", commonId3=" + commonId3 + "]";
-    }
-
-    
 }
